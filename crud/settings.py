428366a242df6dc2644e7bbfb7eb5813bed96ef8
jod35 <jod35@gmail.com>
2020-02-19 12:01:30 -0600

    successful migration to postgres

--- crud/settings.py
+++ crud/settings.py
@@ -56,8 +56,11 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'employeedb',
+        'USER':'jona',
+        'PASSWORD':'nathanoj35',
+        'HOST':'localhost'
     }
 }
 
