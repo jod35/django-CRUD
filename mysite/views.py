a7dd19631a470d387d54023116d386e5eb082c82
jod35 <jod35@gmail.com>
2020-02-20 11:33:52 -0600

    created some views

--- mysite/views.py
+++ mysite/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def employee_list(request):
     return render(request,template_name='')
+
+def add_employees(request):
+    return render(request,template_name='')
+
+def update_employees(request):
+    return render(request,template_name='')
+
+def delete_employees(request):
+    return render(request,template_name='')
+
+    
