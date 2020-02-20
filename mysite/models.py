245139119f9905e291f2afb12e41f684e8a19089
jod35 <jodestrevin@gmail.com>
2020-02-19 21:08:14 -0500

    registered the models

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    position=models.CharField(max_length=200)
+
+class Employee(models.Model):
+    full_name=models.CharField(max_length=200)
+    emp_code=models.CharField(max_length=200)
+    mobile=models.CharField(max_length=200)
+    position=models.ForeignKey(Position,on_delete=models.CASCADE)
